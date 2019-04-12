b4e3049b20709262e4f0c6740f10cb75c1fced13
Martin Rovere <martinr@qb9.net>
2019-04-12 00:41:20 -0300

    agrego un poligono para probar colisiones x fisica

--- assets/tilemaps/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tiles" tilewidth="16" tileheight="16" tilecount="4000" columns="50">
- <image source="tiles/simples_pimples.png" width="800" height="1280"/>
-</tileset>
